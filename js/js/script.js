776f3fd04572f5b7479bfbcc9e3ac04ed89c7fbe
luiza5679 <luiza.kudaiberdieva.bfr@gmail.com>
2024-05-31 13:43:47 +0300

    first commit

---
+++ js/js/script.js
@@ -0,0 +1,116 @@
+// let s=40
+// let t=60
+// let v=s/t
+// console.log(v)
+
+
+// let userNamber=prompt('Атынызды жазынз')
+// let userAge=+prompt('Жашынызды жазыныз')
+// let userYear=2023-userAge
+// console.log('Салам '+userNamber+'! '+'Сиз'+userYear+'жылы туулгансыз!' )
+
+
+// let a=+prompt('Торт бутучтуктун а жагынын узундугу')
+// let b=+prompt('Торт бутучтуктун а жагынын узундугу')
+// let p=(a+b)*2
+// let  s=a*b
+//
+//
+// console.log('торт бурчтуктун пеиметри '+p)
+// console.log('торт бурчтуктун аянты '+s)
+
+
+let a=1
+let b=2
+let c=3
+
+let f = a
+
+a = b
+b = c
+c=f
+
+console.log(a,b,c)
+
+
+
+
+
+// let a=prompt('Ваш данные')
+// let b=+prompt('Ваш возраст')
+// let c=prompt('Ваш город')
+//
+// console.log(`Твои данные ${a}`);
+// console.log(`Твой возраст ${b}`);
+// console.log(`Твой город ${c}`);
+
+// const a=75;
+// const b=112;
+// let userA =+prompt('Сколько у вас сувенир?')
+// let userB=+prompt('Сколько у вас безделушек?')
+// let c=(a*userA)/1000
+// let d=(b*userB)/1000
+
+// console.log(`Ваш сувенир:${c} кг`);
+// console.log(`Ваш безделушек: ${d} кг`);
+
+
+// let a=+prompt('a га маани бериниз')
+// let b=+prompt('b га маани бериниз')
+// let c=a+b
+// let d=a-b
+// let i=a*b
+// let f=a/b
+// let g=a%b
+// console.log(`сумма ${c}`);
+// console.log(`разница ${d}`);
+// console.log(`произведение ${i}`);
+// console.log(`деления ${f}`);
+// console.log(`остаток ${g}`);
+
+
+
+// const a= 24;
+// let userA =+prompt('кунду жаз')
+// let c=a*userA
+// console.log(`${c}`);
+
+
+// let a=+prompt('Гарри');
+// let b=+prompt('Ларри');
+// let c=(a+b)-1;
+//
+// console.log(`${c}`);
+
+
+
+// let a=prompt('Ваш данные')
+// let b=+prompt('Ваш возраст')
+// let c=prompt('Ваш город')
+//
+// document.write( `<p>Твои данные ${a}</p>` );
+// document.write(`<p>Твой возраст ${b}</p>` );
+// document.write( `<p>Твой город ${c}</p>`);
+
+
+
+
+
+//  let a=20;
+// let b=10;
+//
+// if (a>b){
+//  console.log(`${a} саны чон`)
+// }
+//
+// if (a < b) {
+//  console.log(`${b} саны чон`)
+// }
+
+
+
+
+
+
+
+
